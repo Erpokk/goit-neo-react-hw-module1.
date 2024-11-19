5ff50c67c3ea4a785315954b15f274b3b5783ae9
Maksym Adamenko <maimav56@gmail.com>
2024-11-19 23:42:53 +0200

    Hw done

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import Profile from "./Profile/Profile"
+import FriendList from "./FriendList/FriendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import userData from "../userData.json";
+import friends from "../friends.json";
+import transactions from "../transactions.json";
+
+import "modern-normalize";
+import "../index.css";
+
+// Імпорт стилів нормалізації
+
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
+
+export default App;
